324f6813b75726ad7f79573b5a63dafca3af9339
pepo camus <jlcamus@miuandes.cl>
2023-04-25 16:29:00 -0400

    small changes

--- script.js
+++ script.js
@@ -1,11 +1,22 @@
 // Create an XMLHttpRequest object
 const xhttp = new XMLHttpRequest();
+const games = new XMLHttpRequest();
 
 let JSON_OBJECT = {"username":"G11_Camus","password":"password"}
 
 // Define a callback function
 xhttp.onload = function() {
   // Here you can use the Data
+  token = JSON.parse(this.response).access
+  console.log(token)
+
+  games.open("GET","https://trivia-bck.herokuapp.com/api/games/")
+  games.setRequestHeader("Authorization","Bearer "+token)
+  games.send()
+
+}
+
+games.onload = function() {
   console.log(this.response)
 }
 
@@ -13,3 +24,4 @@ xhttp.onload = function() {
 xhttp.open("POST", "https://trivia-bck.herokuapp.com/api/token/");
 xhttp.setRequestHeader("Content-Type","application/json")
 xhttp.send(JSON.stringify(JSON_OBJECT));
+
